85c9218ac4487d8d2a2e2eeb7546e15f0ce26920
Changaco <changaco@changaco.net>
2012-08-27 15:49:22 +0200

    fix import of six.moves

--- miss/six.py
+++ miss/six.py
@@ -165,7 +165,7 @@ for attr in _moved_attributes:
     setattr(_MovedItems, attr.name, attr)
 del attr
 
-moves = sys.modules['six.moves'] = _MovedItems('moves')
+moves = sys.modules['miss.six.moves'] = _MovedItems('moves')
 
 
 def add_move(move):
