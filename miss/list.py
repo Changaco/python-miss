a6809ff0481eb22238d9fb1785622e9f9c6431ef
Changaco <changaco@changaco.net>
2012-09-13 00:24:41 +0200

    add list.grow

--- miss/list.py
+++ miss/list.py
@@ -1,4 +1,19 @@
 
 concat = lambda l: reduce(list.__add__, l, [])
 
+def grow(l, size, filler=None):
+    """Grow the list $l to $size filling with $filler.
+    >>> grow(range(5), 4)
+    [0, 1, 2, 3, 4]
+    >>> grow(range(5), 5)
+    [0, 1, 2, 3, 4]
+    >>> grow(range(5), 6)
+    [0, 1, 2, 3, 4, None]
+    """
+    ll = len(l)
+    if ll < size:
+        return l + [filler]*(size-ll)
+    else:
+        return l
+
 singleton = lambda a: [a]
