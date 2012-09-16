a469396da27bf5cc6f6e42fb66ee5b445cb6fd68
Changaco <changaco@changaco.net>
2012-09-16 16:42:24 +0200

    improve list.grow

--- miss/list.py
+++ miss/list.py
@@ -3,16 +3,21 @@ concat = lambda l: reduce(list.__add__, l, [])
 
 def grow(l, size, filler=None):
     """Grow the list $l to $size filling with $filler.
-    >>> grow(range(5), 4)
+    >>> grow(list(range(5)), 4)
     [0, 1, 2, 3, 4]
-    >>> grow(range(5), 5)
+    >>> grow(list(range(5)), 5)
     [0, 1, 2, 3, 4]
-    >>> grow(range(5), 6)
+    >>> grow(list(range(5)), 6)
     [0, 1, 2, 3, 4, None]
+    >>> grow(list(range(5)), 7, lambda i: i)
+    [0, 1, 2, 3, 4, 5, 6]
     """
     ll = len(l)
     if ll < size:
-        return l + [filler]*(size-ll)
+        if callable(filler):
+            return l + [filler(i) for i in range(ll, size)]
+        else:
+            return l + [filler]*(size-ll)
     else:
         return l
 
