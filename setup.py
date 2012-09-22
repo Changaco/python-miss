5b4bb404fdd41c2a2ef51966bcbd8a4b7dceb8c2
Changaco <changaco@changaco.net>
2012-09-22 18:18:10 +0200

    add README and packaging stuff

---
+++ setup.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+
+from os.path import join, dirname
+
+from setuptools import setup, find_packages
+
+from version import get_version
+
+setup(
+    name='miss',
+    version=get_version(),
+    description='A collection of generic functions and classes to fill the gaps of the stdlib',
+    author='Changaco',
+    author_email='changaco ατ changaco δοτ net',
+    url='http://changaco.net/gitweb/?p=python-miss.git',
+    license='LGPLv3+',
+    packages=find_packages(),
+    long_description=open(join(dirname(__file__), 'README')).read(),
+)
